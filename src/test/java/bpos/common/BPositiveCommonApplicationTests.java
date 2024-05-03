ae1ac50c36f80ef5fe96968b6401d6f6edb01bdb
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-03 23:46:48 +0300

    first commit

---
+++ src/test/java/bpos/common/BPositiveCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package bpos.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BPositiveCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
