01286cab3a0ff3fa34746b36233798bb36ca830a
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-04 11:33:27 +0300

    gradle

---
+++ BPositiveModel/src/test/java/bpos/common/model/BPositiveModelApplicationTests.java
@@ -0,0 +1,13 @@
+package bpos.common.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BPositiveModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
