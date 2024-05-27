39d77e98f55699e91fecc19f0dcec315463c7cad
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-27 20:59:10 +0300

    another one

--- BPositiveOther/src/main/java/bpos/other/PersonRequest.java
+++ BPositiveOther/src/main/java/bpos/other/PersonRequest.java
@@ -105,6 +105,22 @@ public class PersonRequest {
         return city;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setCity(String city) {
         this.city = city;
     }
